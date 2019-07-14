a1e130cf8a21c8d217103bada90e53cad7bf277d
pentium3 <celerond@msn.com>
2019-07-14 16:47:55 -0500

    fix showres

--- showres.py
+++ showres.py
@@ -1,15 +1,29 @@
 
-def showres(FL):
+def showres(FL, OUTNAME):
     from datamodel import GPDataSet
     import pickle
+    fo=open(OUTNAME, 'w')
+
+    FNAME=FL[0]
+    ff=open(FNAME,'rb')
+    ds=pickle.load(ff)
+    for j in ds.knob_labels:
+        fo.write(j+', ')
+    for j in ds.metric_labels:
+        fo.write(j + ', ')
+    fo.write("\n")
+    ff.close()
+
     for FNAME in FL:
         ff=open(FNAME,'rb')
         ds=pickle.load(ff)
         #print(FNAME)
         for i in range(ds.num_previousamples):
-            print(float(ds.previous_knob_set[i]), end=', ')
+            for j in (ds.previous_knob_set[i]):
+                fo.write(str(float(j)) + ', ')
             for j in (ds.previous_metric_set[i]):
-                print(float(j), end=', ')
-            print("")
+                fo.write(str(float(j))+', ')
+            fo.write("\n")
         ff.close()
+    fo.close()
 
