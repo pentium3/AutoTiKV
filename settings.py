cddd35947ad859f1588e37ba6c186b2d46345d99
pentium3 <celerond@msn.com>
2019-08-24 07:55:35 -0500

    shortscan

--- settings.py
+++ settings.py
@@ -9,17 +9,17 @@ wl_metrics={
     "writeheavy":["write_throughput","write_latency","store_size","compaction_cpu"],        #UPDATE
     "pntlookup40": ["get_throughput","get_latency","store_size","compaction_cpu"],          #READ
     "pntlookup80": ["get_throughput","get_latency","store_size","compaction_cpu"],          #READ
-    "longscan":  ["write_throughput","write_latency","scan_throughput","scan_latency","store_size","compaction_cpu"],        #SCAN, INSERT
-    "shortscan": ["write_throughput","write_latency","scan_throughput","scan_latency","store_size","compaction_cpu"],        #SCAN, INSERT
+    "longscan":  ["scan_throughput","scan_latency","store_size","compaction_cpu"],          #SCAN
+    "shortscan": ["scan_throughput","scan_latency","store_size","compaction_cpu"],          #SCAN
     "smallpntlookup": ["get_throughput","get_latency","store_size","compaction_cpu"],       #READ
 }
 # workload to be load
-loadtype = "pntlookup80_load"
+loadtype = "shortscan"
 # workload to be run
-wltype = "pntlookup80"
+wltype = "shortscan"
 
 # only 1 target metric to be optimized
-target_metric_name="get_throughput"
+target_metric_name="scan_latency"
 
 # several knobs to be tuned
 target_knob_set=['rocksdb.writecf.bloom-filter-bits-per-key',
